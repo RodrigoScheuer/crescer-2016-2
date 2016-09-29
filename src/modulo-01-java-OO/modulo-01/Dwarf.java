341f5b406093848595b0525bbb1e5765eed4701f
Unknown <rodrigo.scheuer@cwi.com.br>
2016-09-29 11:24:53 -0300

    Feat(Dwarf,DwarfTest) : adicionar getNumeroSorte(), modificar perderVida(), testes dos métodos.

--- src/modulo-01-java-OO/modulo-01/Dwarf.java
+++ src/modulo-01-java-OO/modulo-01/Dwarf.java
@@ -1,18 +1,58 @@
 public class Dwarf {
+    private String nome;
     private int vida;
+    private int experiencia;
+    private DataTerceiraEra dataNascimento;
     
     // java type initializer
     // vai ser replicado para cada construtor
     {
         vida = 110;
     }
+    
+    public Dwarf(){
+        this.nome = "dwarf";
+        this.dataNascimento = new DataTerceiraEra(1,1,1);
+    }
+    
+    public Dwarf(String nome, DataTerceiraEra data){
+        this.nome = nome;
+        this.dataNascimento = data;
+    }
 
     public void perderVida() {
-        vida -= 10;
+        if(getNumeroSorte() < 0)
+            experiencia += 2;
+            
+        if(getNumeroSorte() > 100)
+            this.vida -= 10;
     }
 
     public int getVida() {
         return vida;
     }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public DataTerceiraEra getDataNascimento(){
+        return this.dataNascimento;
+    }
+    
+    public double getNumeroSorte(){
+        if(dataNascimento.ehBissexto(dataNascimento.getAno()) == true && getVida() >= 80 && getVida() <= 90)
+            return -33 * 101;
+        
+        if(dataNascimento.ehBissexto(dataNascimento.getAno()) == false && getNome() == "Seixas" || getNome() == "Meireles")
+            return (101.0 * 33) % 100;
+            
+        return 101.0;
+    }
+    
+    public int getExperiencia(){
+        return this.experiencia;
+    }
 }
-// Dwarf gimli;
+
+
--- src/modulo-01-java-OO/modulo-01/DwarfTest.java
+++ src/modulo-01-java-OO/modulo-01/DwarfTest.java
@@ -51,6 +51,85 @@ public class DwarfTest
         gimli.perderVida();
         assertEquals(-90, gimli.getVida());
     }
+    
+     @Test
+    public void dwarfNasceComNomeEDataNascimento() {
+        DataTerceiraEra data = new DataTerceiraEra(30,12,1992);
+        Dwarf gimli = new Dwarf("Rodrigo", data);
+        assertEquals("Rodrigo", gimli.getNome());
+        assertEquals(data, gimli.getDataNascimento());
+    }
+    
+     @Test
+    public void dwarfNasce() {
+        Dwarf gimli = new Dwarf();
+        assertEquals(110, gimli.getVida());
+        assertEquals("dwarf", gimli.getNome());
+        assertEquals(1, gimli.getDataNascimento().getDia());
+        assertEquals(1, gimli.getDataNascimento().getMes());
+        assertEquals(1, gimli.getDataNascimento().getAno());
+    }
+    
+    @Test
+    public void dwarfGetNumeroSorteAnoEhBissexto() {
+        double Epsilon = 0.001;
+        DataTerceiraEra data = new DataTerceiraEra(30,12,2012);
+        Dwarf gimli = new Dwarf("Rodrigo", data);
+        gimli.perderVida();
+        gimli.perderVida();
+        assertEquals(-3333.0, gimli.getNumeroSorte(),Epsilon); // não sei se esta correto, calculadora exibe valor diferente
+    }
+    
+    @Test
+    public void dwarfGetNumeroSorteAnoNaoEhBissexto() {
+        double Epsilon = 0.001;
+        DataTerceiraEra data = new DataTerceiraEra(30,12,2013);
+        Dwarf gimli = new Dwarf("Seixas", data);
+        gimli.perderVida();
+        gimli.perderVida();
+        assertEquals(33, gimli.getNumeroSorte(),Epsilon);
+    }
+    
+    @Test
+    public void dwarfSemSorte() {
+        double Epsilon = 0.001;
+        DataTerceiraEra data = new DataTerceiraEra(30,12,2013);
+        Dwarf gimli = new Dwarf("Rodrigo", data);
+        assertEquals(101, gimli.getNumeroSorte(), Epsilon);
+    }
+    
+    @Test
+    public void DwarfNaoLevaFlechada(){
+                                        // ano nao é bissexto
+        DataTerceiraEra data = new DataTerceiraEra(30,12,2013);
+        Dwarf gimli = new Dwarf("Seixas", data);
+        gimli.perderVida();
+        assertEquals(110, gimli.getVida());
+        assertEquals(0, gimli.getExperiencia());
+    }
+    
+    @Test
+    public void DwarfLevaFlechada(){
+                                        // ano não é bissexto
+        DataTerceiraEra data = new DataTerceiraEra(30,12,2013);
+        Dwarf gimli = new Dwarf("Rodrigo", data);
+        gimli.perderVida();
+        assertEquals(100, gimli.getVida());
+        assertEquals(0, gimli.getExperiencia());
+    }
+    
+     @Test
+    public void DwarfNaoLevaFlechadaEGanhaExperiencia(){
+                                        // ano não é bissexto
+        DataTerceiraEra data = new DataTerceiraEra(30,12,2012);
+        Dwarf gimli = new Dwarf("Rodrigo", data);
+        gimli.perderVida();
+        gimli.perderVida();
+        gimli.perderVida();
+        assertEquals(90, gimli.getVida());
+        assertEquals(2, gimli.getExperiencia());
+    }
+        
 }
 
 
