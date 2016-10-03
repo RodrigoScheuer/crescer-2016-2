624fb11def5baf2bea2789717f1ebfd6669ffed7
Unknown <rodrigo.scheuer@cwi.com.br>
2016-10-03 09:24:31 -0300

    Feat(Inventario,InventarioTest, TipoOrdenacao): adicionar metodo de ordenacao de inventario

--- src/modulo-01-java-OO/modulo-01/Inventario.java
+++ src/modulo-01-java-OO/modulo-01/Inventario.java
@@ -3,19 +3,19 @@ import java.util.ArrayList;
 public class Inventario{
 
     private ArrayList<Item> itens = new ArrayList<>();
-    
+
     public ArrayList<Item> getItens(){
         return this.itens; 
     }
-    
+
     public void adicionarItem(Item item){
-             itens.add(item);
+        itens.add(item);
     }
-    
+
     public void removerItem(Item item){
         itens.remove(item);
     }
-    
+
     public String getDescricoesItens(){
         String s = " ";
         for(int i = 0; i < itens.size(); i++){
@@ -23,7 +23,7 @@ public class Inventario{
         }
         return s;
     }
-    
+
     public Item getItemMaiorQuantidade(){
         Item itemMaior = itens.get(0);
         for(int i = 1; i < itens.size(); i++){
@@ -33,5 +33,60 @@ public class Inventario{
         }
         return itemMaior;
     }
-    
+
+    public void ordenarItens(){         // *talvez da pra otimizar
+        Item itemAtual, proximoItem;
+        boolean achou = true; // começa true pra entrar no metodo
+        while(achou == true){
+            achou = false;
+            for(int i = 0; i < itens.size(); i++){
+                itemAtual = getItens().get(i);
+                for(int j = 1; j < itens.size(); j++){
+                    proximoItem = getItens().get(j);
+                    if(i < j){ // para não fazer troca errada
+                        if(itemAtual.getQuantidade() > proximoItem.getQuantidade()){
+                            getItens().set(j, itemAtual);
+                            getItens().set(i, proximoItem);
+                            achou = true;
+                            break; // sai do for
+                        }
+                    }
+                }
+                if(achou == true){
+                    break;  // sai do segundo for
+                }
+            }
+        }
+    }
+
+    public void ordenarItens(TipoOrdenacao tipo){
+        if(tipo == TipoOrdenacao.ASCENDENTE){
+            ordenarItens();
+        }else{
+            Item itemAtual, proximoItem;
+            boolean achou = true; // começa true pra entrar no metodo
+            while(achou == true){
+                achou = false;
+                for(int i = 0; i < itens.size(); i++){
+                    itemAtual = getItens().get(i);
+                    for(int j = 1; j < itens.size(); j++){
+                        proximoItem = getItens().get(j);
+                        if(i < j){ // para não fazer troca errada
+                            if(itemAtual.getQuantidade() < proximoItem.getQuantidade()){
+                                getItens().set(j, itemAtual);
+                                getItens().set(i, proximoItem);
+                                achou = true;
+                                break; // sai do for
+                            }
+                        }
+                    }
+                    if(achou == true){
+                        break;  // sai do segundo for
+                    }
+                }
+            }
+        }
+
+    }
 }
+
--- src/modulo-01-java-OO/modulo-01/InventarioTest.java
+++ src/modulo-01-java-OO/modulo-01/InventarioTest.java
@@ -63,5 +63,115 @@ public class InventarioTest
      mochila.adicionarItem(item02);
      assertEquals(2, mochila.getItemMaiorQuantidade().getQuantidade());
  }
+ 
+ @Test
+ public void ordena4ItensAscendentes(){
+     Item item01 = new Item("Espada", 2);
+     Item item02 = new Item("Escudo", 1);
+     Item item03 = new Item("poção vida", 20);
+     Item item04 = new Item("poção mana", 10);
+     Inventario mochila = new Inventario();
+     mochila.adicionarItem(item01);
+     mochila.adicionarItem(item02);
+     mochila.adicionarItem(item03);
+     mochila.adicionarItem(item04);
+     mochila.ordenarItens();
+     assertEquals(1, mochila.getItens().get(0).getQuantidade());
+     assertEquals(2, mochila.getItens().get(1).getQuantidade());
+     assertEquals(10, mochila.getItens().get(2).getQuantidade());
+     assertEquals(20, mochila.getItens().get(3).getQuantidade());
+ } 
+ 
+  @Test
+ public void ordena5ItensAscendentesMenorNaUltimaPosicao(){
+     Item item01 = new Item("Espada", 2);
+     Item item02 = new Item("Escudo", 1);
+     Item item03 = new Item("poção vida", 20);
+     Item item04 = new Item("poção mana", 10);
+     Item item05 = new Item("Capacete", 0);
+     Inventario mochila = new Inventario();
+     mochila.adicionarItem(item01);
+     mochila.adicionarItem(item02);
+     mochila.adicionarItem(item03);
+     mochila.adicionarItem(item04);
+     mochila.adicionarItem(item05);
+     mochila.ordenarItens();
+     assertEquals(0, mochila.getItens().get(0).getQuantidade());
+     assertEquals(1, mochila.getItens().get(1).getQuantidade());
+     assertEquals(2, mochila.getItens().get(2).getQuantidade());
+     assertEquals(10, mochila.getItens().get(3).getQuantidade());
+     assertEquals(20, mochila.getItens().get(4).getQuantidade());
+ }
+ 
+ @Test
+ public void ordena10ItensAscendentes(){
+     Item item01 = new Item("Espada", 2);
+     Item item02 = new Item("Escudo", 1);
+     Item item03 = new Item("poção vida", 20);
+     Item item04 = new Item("poção mana", 10);
+     Item item05 = new Item("Capacete", 0);
+     Item item06 = new Item("Espada", 2);
+     Item item07 = new Item("Escudo", 1);
+     Item item08 = new Item("poção vida", 20);
+     Item item09 = new Item("poção mana", 10);
+     Item item10 = new Item("Capacete", 0);
+     Inventario mochila = new Inventario();
+     mochila.adicionarItem(item01);
+     mochila.adicionarItem(item02);
+     mochila.adicionarItem(item03);
+     mochila.adicionarItem(item04);
+     mochila.adicionarItem(item05);
+     mochila.adicionarItem(item06);
+     mochila.adicionarItem(item07);
+     mochila.adicionarItem(item08);
+     mochila.adicionarItem(item09);
+     mochila.adicionarItem(item10);
+     mochila.ordenarItens();
+     assertEquals(0, mochila.getItens().get(0).getQuantidade());
+     assertEquals(0, mochila.getItens().get(1).getQuantidade());
+     assertEquals(1, mochila.getItens().get(2).getQuantidade());
+     assertEquals(1, mochila.getItens().get(3).getQuantidade());
+     assertEquals(2, mochila.getItens().get(4).getQuantidade());
+     assertEquals(2, mochila.getItens().get(5).getQuantidade());
+     assertEquals(10, mochila.getItens().get(6).getQuantidade());
+     assertEquals(10, mochila.getItens().get(7).getQuantidade());
+     assertEquals(20, mochila.getItens().get(8).getQuantidade());
+     assertEquals(20, mochila.getItens().get(9).getQuantidade());
+ }
 
+ @Test
+ public void ordena10ItensDescendentes(){
+     Item item01 = new Item("Espada", 2);
+     Item item02 = new Item("Escudo", 1);
+     Item item03 = new Item("poção vida", 20);
+     Item item04 = new Item("poção mana", 10);
+     Item item05 = new Item("Capacete", 0);
+     Item item06 = new Item("Espada", 2);
+     Item item07 = new Item("Escudo", 1);
+     Item item08 = new Item("poção vida", 20);
+     Item item09 = new Item("poção mana", 10);
+     Item item10 = new Item("Capacete", 0);
+     Inventario mochila = new Inventario();
+     mochila.adicionarItem(item01);
+     mochila.adicionarItem(item02);
+     mochila.adicionarItem(item03);
+     mochila.adicionarItem(item04);
+     mochila.adicionarItem(item05);
+     mochila.adicionarItem(item06);
+     mochila.adicionarItem(item07);
+     mochila.adicionarItem(item08);
+     mochila.adicionarItem(item09);
+     mochila.adicionarItem(item10);
+     mochila.ordenarItens(TipoOrdenacao.DESCENDENTE);
+     assertEquals(20, mochila.getItens().get(0).getQuantidade());
+     assertEquals(20, mochila.getItens().get(1).getQuantidade());
+     assertEquals(10, mochila.getItens().get(2).getQuantidade());
+     assertEquals(10, mochila.getItens().get(3).getQuantidade());
+     assertEquals(2, mochila.getItens().get(4).getQuantidade());
+     assertEquals(2, mochila.getItens().get(5).getQuantidade());
+     assertEquals(1, mochila.getItens().get(6).getQuantidade());
+     assertEquals(1, mochila.getItens().get(7).getQuantidade());
+     assertEquals(0, mochila.getItens().get(8).getQuantidade());
+     assertEquals(0, mochila.getItens().get(9).getQuantidade());
+    }
 }
