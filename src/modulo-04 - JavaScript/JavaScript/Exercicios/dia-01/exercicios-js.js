8c8a26afcf2be19795a32fa13858cf4092fbea79
Unknown <rodrigo.scheuer@cwi.com.br>
2016-10-25 11:57:37 -0200

    refactor(exercicios) : salvar tentativas fracassadas dos exercicios 4 , 5 para aula.

--- src/modulo-04 - JavaScript/JavaScript/Exercicios/dia-01/exercicios-js.js
+++ src/modulo-04 - JavaScript/JavaScript/Exercicios/dia-01/exercicios-js.js
@@ -76,10 +76,8 @@ console.log(find([ { nome: 'a' }, { nome: 'b' }  ], function(elem) {
 }));
 // []
 
-/*
-var maiorIgualADois = 'maior igual ou a dois';
-console.log(find([ 1, 2, 3 ], maiorIgualADois));
-// 0 */
+var notFunction = 'maior igual ou a dois';
+// 0
 
 describe('find(busca Maior Igual A Dois)', function() {
   it('[1,2,3] deve retornar 2,3', function() {
@@ -106,11 +104,60 @@ describe('find(busca Maior Igual A Dois)', function() {
 
       describe('find(Segundo parametro não é função)', function() {
         it('[1,2,3] deve retornar 0', function() {
-          var resultado = find([ 1, 2, 3 ], 'maior igual ou a dois');
+          var resultado = find([ 1, 2, 3 ], notFunction);
           expect(resultado).toEqual(0);
         })});
 
 /*---------------------------------------------------------*/
-console.log(" ");
+/*console.log(" ");
 console.log("-- Exercicio 4 --");
 console.log(" ");
+//  não consigui fazer
+
+function subtrair(num1)(num2) {
+    return num1 - num2;
+}
+
+console.log(subtrair(2)(1)); // 1
+console.log(subtrair(0)(0)); // 0
+console.log(subtrair(-1)(2)); // -3 */
+
+
+/*---------------------------------------------------------*/
+console.log(" ");
+console.log("-- Exercicio 5 --");
+console.log(" ");
+
+
+function iguais(par1, par2){
+  if(typeof par1 === 'object' && typeof par2 === 'object'){
+    if(par1 === par2){
+      return true;
+    }else{
+      return false;
+      // fazer comparação de objetos aki
+      }
+  }
+}
+
+console.log(" ");
+
+var obj = { a: { a: 'a' }, b: 2 };
+console.log(iguais(obj, obj)); // true
+console.log(iguais(obj, { a: 1, b: 2 })); // false
+console.log(iguais(obj, { a: { a: 'a' }, b: 2 })); // true
+console.log(iguais({ a: 1 }, { b: 1 })); // false
+
+console.log("-----------------------------------------------------");
+
+var goku = {
+  nivel: 'SSJ4',
+  golpes: [ { nome: 'kamehameha', dano: 45 }, { nome: 'genki-dama', dano: 99 } ],
+  nome: 'Goku',
+}, sonGoku = {
+  nome: 'Goku',
+  nivel: 'SSJ4',
+  golpes: [ { nome: 'kamehameha', dano: 45 }, { nome: 'genki-dama', dano: 99 } ]
+};
+console.log(iguais(goku, sonGoku)); // true
+console.log(iguais({ a: 1 }, { a: 1, b: 2 })); // false
