f21734ab5bd8b3c3bab4438c203b77d341c3a503
Unknown <rodrigo.scheuer@cwi.com.br>
2016-10-26 11:02:47 -0200

    refactor(exercicio1-6) : exercicios prontos, falta testes.

--- src/modulo-04 - JavaScript/JavaScript/Exercicios/dia-01 - tema/exercicio1-6.js
+++ src/modulo-04 - JavaScript/JavaScript/Exercicios/dia-01 - tema/exercicio1-6.js
@@ -1,91 +1,91 @@
-  console.log("Array marvel");
-  console.log(marvel);
-  console.log("");
+console.log("Array marvel");
+console.log(marvel);
+console.log("");
 
-  class Herois {
-    constructor(array){
-        this.heroi = array;
-  }
+class Herois {
+  constructor(array){
+      this.heroi = array;
+}
 
-    foraGuerraCivil(){
-      let pacificos = [];
-      let cont = 0;
-      let eventos = [];
-      for (let i = 0, len = this.heroi.length; i < len; i++) {
-        eventos = this.heroi[i].events.items;
-        cont = 0;
-        for (let j = 0, len = eventos.length; j < len; j++) {
-          if(eventos[j].name === 'Civil War'){
-              cont++;
-          }
-        }
-        if(cont === 0){
-        pacificos.push(this.heroi[i]);
+  foraGuerraCivil(){
+    let pacificos = [];
+    let cont = 0;
+    let eventos = [];
+    for (let i = 0, len = this.heroi.length; i < len; i++) {
+      eventos = this.heroi[i].events.items;
+      cont = 0;
+      for (let j = 0, len = eventos.length; j < len; j++) {
+        if(eventos[j].name === 'Civil War'){
+            cont++;
         }
       }
-    return pacificos;
-  }
-
-
-  maisPublicado(){
-    let personagem;
-    let maior = this.heroi[0].comics.available;
-    for (let i = 1, len = this.heroi.length; i < len; i++) {
-      if(this.heroi[i].comics.available > maior){
-         maior = this.heroi[i].comics.available;
-         personagem = this.heroi[i];
+      if(cont === 0){
+      pacificos.push(this.heroi[i]);
       }
     }
-    return personagem;
-  }
+  return pacificos;
+}
 
-  mediaPaginas(){
-    let media = 0;
-    let tamanhoQuadrinhos = 0;
-    for (let i = 0, len = this.heroi.length; i < len; i++) {
-       tamanhoQuadrinhos = this.heroi[i].comics.items.length;
-      for (let j = 0, len = tamanhoQuadrinhos ; j < len; j++) {
-         media += this.heroi[i].comics.items[j].pageCount;
+
+maisPublicado(){
+  let personagem;
+  let maior = this.heroi[0].comics.available;
+  for (let i = 1, len = this.heroi.length; i < len; i++) {
+    if(this.heroi[i].comics.available > maior){
+       maior = this.heroi[i].comics.available;
+       personagem = this.heroi[i];
+    }
+  }
+  return personagem;
+}
+
+mediaPaginas(){
+  let media = 0;
+  let tamanhoQuadrinhos = 0;
+  for (let i = 0, len = this.heroi.length; i < len; i++) {
+     tamanhoQuadrinhos = this.heroi[i].comics.items.length;
+    for (let j = 0, len = tamanhoQuadrinhos ; j < len; j++) {
+       media += this.heroi[i].comics.items[j].pageCount;
+    }
+  }
+  return media/this.heroi.length;
+}
+
+seriesPorLongevidade(){
+  let arraySeries = [];
+  let tempoDuracao = 0;
+  let tamanhoSeries = 0;
+  for (let i = 0, len = this.heroi.length; i < len; i++) {
+     tamanhoSeries = this.heroi[i].series.items.length;
+    for (let j = 0, len = tamanhoSeries ; j < len; j++) {
+      tempoDuracao = (this.heroi[i].series.items[j].endYear)  - (this.heroi[i].series.items[j].startYear);
+      arraySeries.push(tempoDuracao);
+      arraySeries.sort(function(a, b){return b-a});
+    }
+  }
+  return arraySeries;
+}
+
+comicMaisCara(){
+  let maior = 0;
+  let soma = 0;
+  let tamanhoQuadrinhos = 0;
+  let tamanhoPrices = 0;
+  for (let i = 0, len = this.heroi.length; i < len; i++) {
+    tamanhoQuadrinhos = this.heroi[i].comics.items.length;
+    soma = 0;
+    for (let j = 0, len = tamanhoQuadrinhos; j < len; j++) {
+        tamanhoPrices = this.heroi[i].comics.items[j].prices.length;
+      for (let l = 0, len = tamanhoPrices; l < len; l++) {
+          soma += this.heroi[i].comics.items[j].prices[l].price;
       }
     }
-    return media/this.heroi.length;
-  }
-
-  seriesPorLongevidade(){
-    let arraySeries = [];
-    let tempoDuracao = 0;
-    let tamanhoSeries = 0;
-    for (let i = 0, len = this.heroi.length; i < len; i++) {
-       tamanhoSeries = this.heroi[i].series.items.length;
-      for (let j = 0, len = tamanhoSeries ; j < len; j++) {
-        tempoDuracao = (this.heroi[i].series.items[j].endYear)  - (this.heroi[i].series.items[j].startYear);
-        arraySeries.push(tempoDuracao);
-        arraySeries.sort(function(a, b){return b-a});
-      }
+    if(soma > maior){
+      maior = soma;
     }
-    return arraySeries;
   }
-
-  comicMaisCara(){
-    let maior = 0;
-    let soma = 0;
-    let tamanhoQuadrinhos = 0;
-    let tamanhoPrices = 0;
-    for (let i = 0, len = this.heroi.length; i < len; i++) {
-      tamanhoQuadrinhos = this.heroi[i].comics.items.length;
-      soma = 0;
-      for (let j = 0, len = tamanhoQuadrinhos; j < len; j++) {
-          tamanhoPrices = this.heroi[i].comics.items[j].prices.length;
-        for (let l = 0, len = tamanhoPrices; l < len; l++) {
-            soma += this.heroi[i].comics.items[j].prices[l].price;
-        }
-      }
-      if(soma > maior){
-        maior = soma;
-      }
-    }
-    return maior;
-  }
+  return maior;
+}
 
 
 }
