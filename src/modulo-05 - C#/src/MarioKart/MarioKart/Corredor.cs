a9184f7f0f912512c54d6d0f7a66f0d256610518
Unknown <rodrigo.scheuer@cwi.com.br>
2016-10-27 17:40:37 -0200

    feat(MarioKart) : implementar tarefa - iniciando;

---
+++ src/modulo-05 - C#/src/MarioKart/MarioKart/Corredor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart
+{
+    public enum NivelCorredor
+    {
+        Noob, Mediano, Profissional
+    };
+
+    public class Corredor
+    {   
+        // enum dentro da classe       
+        public String Nome { get; }
+        public NivelCorredor Nivel { get; set; }
+
+        public Corredor(String nome, NivelCorredor nivel)
+        {
+            this.Nome = nome;
+            this.Nivel = nivel;
+
+        }
+
+    }
+}
---
+++ src/modulo-05 - C#/src/MarioKart/MarioKart/Kart.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart
+{
+    public class Kart
+    {
+        public Corredor Corredor { get; set; }
+        public int Velocidade { get; private set; }
+        protected List<IEquipamento> Equipamentos { get; set; }
+
+        public Kart(Corredor corredor)
+        {
+            this.Corredor = corredor;
+            this.Equipamentos = new List<IEquipamento>();
+        }
+
+        public void Equipar(IEquipamento equipamento)
+        {
+            this.Equipamentos.Add(equipamento);
+        }
+
+        public void CalcularVelocidade()
+        {
+            this.Velocidade = 3 + SomaBonusEquipamentos() + BonusHabilidadeCorredor();
+        }
+
+        private int BonusHabilidadeCorredor()
+        {
+            if (Corredor.Equals("Noob"))
+            {
+                return 3;
+            }
+            else if (Corredor.Equals("Mediano"))
+            {
+                return 5;
+            }
+            return 6 + Equipamentos
+        
+
+        public int SomaBonusEquipamentos()
+        {
+            return 1;
+        }
+
+    }
+}
