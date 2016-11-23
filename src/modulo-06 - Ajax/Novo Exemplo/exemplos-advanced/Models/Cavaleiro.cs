84a0c8f16e538b5c10e3d2c18b77e7ab85bc66d2
Unknown <rodrigo.scheuer@cwi.com.br>
2016-11-23 20:58:58 -0200

    feat(Ajax - exemplo) : salvar exemplo bernardo.

---
+++ src/modulo-06 - Ajax/Novo Exemplo/exemplos-advanced/Models/Cavaleiro.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace exemplos_advanced.Models
+{
+    public class Cavaleiro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string[] Golpes { get; set; }
+
+        public static Cavaleiro Obter(int id)
+        {
+            var cavaleiros = new Dictionary<int, Cavaleiro>()
+            {
+                { 1,  new Cavaleiro { Id = id, Nome = "Seiya", Golpes = new[] { "Metoro de Pegasus", "Centelha de Pegasus" } } },
+                { 2,  new Cavaleiro { Id = id, Nome = "Shiryu", Golpes = new[] { "Cólera do Dragão", "Cólera dos 100 dragões" } } },
+            };
+
+            return cavaleiros[id];
+        }
+    }
+}
