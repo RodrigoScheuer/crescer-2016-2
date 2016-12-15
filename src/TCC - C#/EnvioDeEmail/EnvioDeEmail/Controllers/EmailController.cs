15a3a7543d58ef445fd934120dae9d9bbf800c5f
Unknown <rodrigo.scheuer@cwi.com.br>
2016-12-15 11:56:11 -0200

    feat(EnvioDeEmail): enviar email via smtp.

---
+++ src/TCC - C#/EnvioDeEmail/EnvioDeEmail/Controllers/EmailController.cs
@@ -0,0 +1,30 @@
+﻿using EnvioDeEmail.Models;
+using System.Web.Mvc;
+
+namespace EnvioDeEmail.controller
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public ActionResult enviarEmail(emailModel model)
+        {
+            model.enviarEmail(model.para, model.assunto, model.mensagem);
+            if (model.teste == true)
+            {
+                TempData["mensagemSucesso"] = "enviado com sucesso!";
+            }
+            TempData["mensagemErro"] = "erro";
+            return View("index");
+        }
+    }
+}
---
+++ src/TCC - C#/EnvioDeEmail/EnvioDeEmail/Models/emailModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace EnvioDeEmail.Models
+{
+    public class emailModel
+    {
+        public string para { get; set; }
+        public string assunto { get; set; }
+        public string mensagem { get; set; }
+        public bool teste { get; set; }
+
+        public object enviarEmail(string para, string assunto, string mensagem)
+        {
+            try
+            {
+                // Hotmail                                          gmail
+                //smtp.Host = "smtp.live.com";                  smtp.Host = "smtp.gmail.com";
+                //smtp.Port = 587;                              smtp.Port = 465;
+                //email:rodrigo.scheuer@hotmail.com             email:  emailTesteCwi@gmail.com
+                //                                              senha: GAoXIP3tC0Qv
+
+                teste = false;
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                MailMessage mail = new MailMessage("emailTesteCwi@gmail.com", para, assunto, mensagem);
+
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("emailTesteCwi@gmail.com", "GAoXIP3tC0Qv");
+
+                if (!string.IsNullOrWhiteSpace(para))
+                {
+                    teste = true;
+                }
+                else
+                {
+                    teste = false;
+                }
+
+                smtp.Send(mail);
+                return teste;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public static bool ValidaEnderecoEmail(string enderecoEmail)
+        {
+            Regex expressaoRegex = new Regex(@"\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}");
+
+            if (expressaoRegex.IsMatch(enderecoEmail))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
